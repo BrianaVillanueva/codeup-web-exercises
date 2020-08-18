f5983039ec903e3199a048bac1602e8b478c139d
Briana Villanueva <briana@Brianas-Air.satx.rr.com>
2020-08-17 19:23:13 -0500

    test

---
+++ js/require.js
@@ -0,0 +1,10 @@
+const data = require('./data.js');
+
+console.log(data.puppy);
+
+
+const data = require('./data.js');
+
+
+// console.log(data.obj.whichSideOfTheForce); // outputs "The Dark Side"
+// console.log(data.multiply(4,5));
