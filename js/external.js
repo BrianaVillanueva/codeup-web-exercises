e36fcc84d2d31250dfb17499ef59486e8f21b3f2
Briana Villanueva <briana@brianas-air.home>
2020-09-03 16:55:37 -0500

    test2

--- js/external.js
+++ js/external.js
@@ -6,6 +6,7 @@ alert("Welcome to my website");
 var userColor = prompt("What is your favorite color?");
 var alertMessage = "Great, " + userColor + " is my favorite color too!";
 alert(alertMessage);
+alertMessage
 
 
 //3.1 You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days, they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a movie per day is $3, how much will you have to pay?
